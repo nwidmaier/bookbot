7cc4ceb701be9e5dd2ca7b48a9497b79f43821f7
nwidmaier <nwidmaier@gmail.com>
2025-05-17 14:49:07 -0700

    updating main.py with count words

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 def main():
       book_path = "books/frankenstein.txt"
       current_book = get_book_text(book_path)
-      print(current_book)
+      print(count_words(current_book))
 
 def get_book_text(filepath): 
         with open(filepath) as book_file:
             return(book_file.read())
+        
+def count_words(current_book):
+    raw_words = current_book.split()
+    word_count= 0
+
+    for i in raw_words:
+        word_count += 1
+    return(word_count)    
+      
 
 main()
